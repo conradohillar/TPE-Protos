87e9ce56bd554d6d9db3a8cd6a3443868ed06b94
conradohillar <hillarconrado@gmail.com>
2025-07-14 01:13:53 -0300

    More bugfixing

--- src/server/socks5/socks5.c
+++ src/server/socks5/socks5.c
@@ -44,20 +44,24 @@ void socksv5_passive_accept(struct selector_key* key) {
 
     conn->s = key->s;
     conn->client_fd = fd;
-    conn->in_buff_data = malloc(get_server_data()->buffer_size);
+
+    size_t buffer_size = get_server_data()->buffer_size;
+    LOG(DEBUG, "Buffer size: %zu", buffer_size);
+
+    conn->in_buff_data = malloc(buffer_size);
     if(conn->in_buff_data == NULL) {
         LOG_MSG(ERROR, "Failed to allocate memory for input buffer data");
         selector_unregister_fd(key->s, fd);
         return;
     }
-    conn->out_buff_data = malloc(get_server_data()->buffer_size);
+    conn->out_buff_data = malloc(buffer_size);
     if(conn->out_buff_data == NULL) {
         LOG_MSG(ERROR, "Failed to allocate memory for output buffer data");
         selector_unregister_fd(key->s, fd);
         return;
-    }   
-    buffer_init(&conn->in_buff, SOCKS5_BUFF_MAX_LEN, conn->in_buff_data);
-    buffer_init(&conn->out_buff, SOCKS5_BUFF_MAX_LEN, conn->out_buff_data);
+    }
+    buffer_init(&conn->in_buff, buffer_size, conn->in_buff_data);
+    buffer_init(&conn->out_buff, buffer_size, conn->out_buff_data);
     
     conn->stm = socks5_stm_init();
     if (conn->stm == NULL) {
@@ -95,7 +99,6 @@ static void socksv5_read(struct selector_key* key) {
             LOG(ERROR, "Error in SOCKS5 state machine for fd %d", key->fd);
             metrics_inc_errors(get_server_data()->metrics);
         }
-
     } else if (n_read == 0) {
         LOG(INFO, "Connection closed by client on fd %d", key->fd);
         if(conn->origin_fd > 0) {
@@ -123,20 +126,32 @@ void socksv5_write(struct selector_key* key) {
         return;
     }
 
+    bool full_buffer = conn->origin_fd > 0 && !buffer_can_write(&conn->out_buff);
+
     size_t n;
     uint8_t* read_ptr = buffer_read_ptr(&conn->out_buff, &n);
 
     ssize_t n_written = send(conn->client_fd, read_ptr, n, MSG_DONTWAIT);
 
+    
     if (n_written > 0) {
         LOG(DEBUG, "Wrote %zd bytes to fd %d", n_written, conn->client_fd);
         metrics_add_bytes(get_server_data()->metrics, n_written);
         buffer_read_adv(&conn->out_buff, n_written);
-        if (!buffer_can_read(&conn->out_buff)) {
-            selector_set_interest(key->s, conn->client_fd, OP_READ);
-            buffer_reset(&conn->out_buff);
+        
+        if (buffer_can_read(&conn->out_buff)) {
+            selector_set_interest(key->s, conn->client_fd, OP_WRITE);
         }
-        if (buffer_can_write(&conn->in_buff)) { selector_set_interest(key->s, conn->client_fd, OP_READ); }
+        else {
+            selector_set_interest(key->s, conn->client_fd, OP_READ);
+        }
+        
+        // if (buffer_can_write(&conn->in_buff)) { selector_set_interest(key->s, conn->client_fd, OP_READ); }
+        if (buffer_can_read(&conn->in_buff)) { selector_set_interest(key->s, conn->origin_fd, OP_WRITE); }
+        if (full_buffer) {
+            selector_set_interest(key->s, conn->origin_fd, OP_READ);
+        }
+        buffer_compact(&conn->out_buff);
         if(has_write_handler(conn->stm->current->state)){
             stm_handler_write(conn->stm, key);
         }
